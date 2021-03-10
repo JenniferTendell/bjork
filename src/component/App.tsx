ffba0f6deca1e9a97a95dce0f8139952714d659c
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-10 17:31:05 +0100

    created a box for carousel and for grid

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,5 +1,4 @@
 import Layout from "./layout";
-import React from "react";
 import { Grommet } from "grommet";
 
 
--- src/component/masterView.tsx
+++ src/component/masterView.tsx
@@ -1,11 +1,39 @@
-import ProductItem from "./productItem"
+import {  Box, Grid, Grommet } from 'grommet';
+
 
 function MasterView() {
     return(
-        <div>
-            <ProductItem />
-        </div>
+        <Grommet>
+            <Box // Bildspelet
+                background='#C4C4C4'
+                pad='medium'
+                height='20rem'
+            />
+
+            <Box // Produkterna 
+                background='#white'
+                pad='medium'
+                height='20rem'>
+                    
+                <Grid // Lägg in i fler rows. 
+                    rows={['xxsmall', 'small']} // första värdet i row flyttar första sidan mot 0
+                                                //andra värdet flyttar andra sidan mot 0, i y-led.
+                    columns={['xxsmall', 'medium']} // första flyttar första hörnet i x-led mot 0,
+                                                // andra värdet flyttar andra hörnet mot 0.
+                    gap='small'
+                    areas={[
+                        { name: 'main', start: [1, 1], end: [1, 1]},
+                    ]}
+                    >
+                    <Box gridArea='main' background='light-4' />
+
+                </Grid>
+            </Box>
+
+        </Grommet>
+    
     )
+
 }
 
-export default MasterView
+export default MasterView;
