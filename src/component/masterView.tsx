7d91d96f6daba83720b1a0f0b6290aeed6396ca0
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-11 15:00:40 +0100

    styled images

--- src/component/masterView.tsx
+++ src/component/masterView.tsx
@@ -1,43 +1,32 @@
-import {  Box, Grid, Grommet } from 'grommet';
+import { Box, Grid, Grommet } from 'grommet';
 import { Component } from 'react';
 import { products } from './interfaceProducts';
 import ProductItem from './productItem';
 
 class MasterView extends Component {
     render() {
-        return(
+        return (
             <Grommet>
                 <Box // Bildspelet
                     background='#C4C4C4'
                     pad='medium'
                     height='20rem'
                 />
-    
-                <Box // Produkterna 
-                    background='#white'
-                    pad='medium'
-                    height='20rem'
-                    >
-                    <Grid // Lägg in i fler rows. 
-                        rows={['medium', 'medium']} // första värdet i row flyttar första sidan mot 0
-                        //andra värdet flyttar andra sidan mot 0, i y-led.
-                        columns={['medium', 'medium']} // första flyttar första hörnet i x-led mot 0,
-                        // andra värdet flyttar andra hörnet mot 0.
+
+                    <Grid
+                        rows={
+                            ["small", "..."]
+                        }
+                        columns={
+                            { "count": "fit", "size": "medium" }
+                        }
                         gap='small'
                     >
-
-                        {products.map((product) =>
-                            <ProductItem product={product} />
+                        {products.map((product, index) =>
+                            <ProductItem product={product}
+                                key={index} />
                         )}
-
-
-                        <Box  
-                            direction='column'
-                            background='light-4' 
-                            /> 
                     </Grid>
-                </Box>
-
             </Grommet>
         );
     }
--- src/component/productItem.tsx
+++ src/component/productItem.tsx
@@ -1,17 +1,21 @@
-import { Product } from "./interfaceProducts"
+import { Product } from "./interfaceProducts";
+import './CSS/productItem.css';
+import { Box } from "grommet";
 
 interface Props {
     product: Product
 }
 
 function ProductItem(props: Props) {
-    const { title, image } = props.product; 
+    const { title, image } = props.product;
 
-    return(
-        <div>
+    return (
+    <Box direction='column' >
+            <img className='image-size' src={image} alt="" />
+        <Box height='xsmall' width='medium'>
             <h2>{title}</h2>
-            <img src={image} alt="" />
-        </div>
+        </Box>
+    </Box>
     )
 }
 
