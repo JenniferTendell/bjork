771ec71e2684140b5e4e433651b6516712994afa
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-12 13:51:07 +0100

    cleaned up code

--- src/component/MasterView-Components/topSellers.tsx
+++ src/component/MasterView-Components/topSellers.tsx
@@ -2,20 +2,6 @@ import { Box, Carousel, Grommet } from 'grommet';
 import { Component, CSSProperties } from 'react';
 import { products } from '../interfaceProducts';
 
-// const customTheme = {
-//     carousel: {
-//         animation: {
-//             duration: 400,
-//         },
-//         icons: {
-//             color: '#37513B',
-//         },
-//         disabled: {
-//             color: '#708C7E'
-//         }
-//     }
-// }
-
 const rootImagesSize: CSSProperties = {
     width: '35rem',
     height: '30rem',
@@ -23,32 +9,40 @@ const rootImagesSize: CSSProperties = {
 }
 
 class TopSellers extends Component {
-    static parameters: {chromatic: { disable: boolean;}; };
+    static parameters: { chromatic: { disable: boolean; }; };
     render() {
-        return(
+        return (
             <Grommet>
                 <Box align='center' pad='medium'>
                     <Carousel fill>
-                        <Box align='center'>
-                            <img
-                                style={ rootImagesSize }
-                                src={products[8]?.image} alt='' />
-                        </Box>
-                        <Box align='center'>
-                            <img
-                                style={ rootImagesSize }
-                                src={products[10]?.image} alt='' />
-                        </Box>
-                        <Box align='center'>
-                            <img
-                                style={ rootImagesSize }
-                                src={products[11]?.image} alt='' />
-                        </Box>
-                        <Box align='center'>
-                            <img
-                                style={ rootImagesSize }
-                                src={products[6]?.image} alt='' />
-                        </Box>
+                            <Box align='center'>
+                                <img
+                                    style={rootImagesSize}
+                                    src={products[8]?.image}
+                                    alt=''
+                                />
+                            </Box>
+                            <Box align='center'>
+                                <img
+                                    style={rootImagesSize}
+                                    src={products[10]?.image}
+                                    alt=''
+                                />
+                            </Box>     
+                            <Box align='center'>
+                                <img
+                                    style={rootImagesSize}
+                                    src={products[11]?.image}
+                                    alt=''
+                                />
+                            </Box>       
+                            <Box align='center'>
+                                <img
+                                    style={rootImagesSize}
+                                    src={products[6]?.image}
+                                    alt=''
+                                />
+                            </Box>
                     </Carousel>
                 </Box>
             </Grommet>
@@ -57,7 +51,7 @@ class TopSellers extends Component {
 }
 
 TopSellers.parameters = {
-    chromatic:  {disable: false},
+    chromatic: { disable: false },
 };
 
 export default TopSellers;
