d03600f9e403d1b96463ba64413669c51669db2e
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-03-12 16:57:57 +0100

    Layout masterView and productItem.

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <Grommet plain>
+      <Grommet>
         <Layout />
       </Grommet>
     </BrowserRouter>
