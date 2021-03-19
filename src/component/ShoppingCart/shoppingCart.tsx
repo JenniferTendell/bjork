30b908264da32316834d84cd638fb64f42dab6e0
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-19 14:31:45 +0100

    added function to get total amount of items

--- src/component/ShoppingCart/shoppingCart.tsx
+++ src/component/ShoppingCart/shoppingCart.tsx
@@ -5,44 +5,66 @@ import ShoppingItem from './shoppingItem';
 
 
 function ShoppingCart() {
-    const { cart } = useContext(CartContext)
+    const { cart } = useContext(CartContext);
+    let totalSum: number = 0;
+
+
+    const getTotalSum = (totalSum: number) => {
+
+        const cartPrices = [];
+        
+        for (const item of cart) {
+            if (item.quantity > 1) {
+                const sum = item.quantity * item.price;
+                cartPrices.push(sum);
+            } else {
+                cartPrices.push(item.price)
+            }
+        }
+        
+        for (const price of cartPrices) {
+            totalSum += price
+        }
+        return totalSum;
+    }
+    
 
     return (
         <Box
-            margin={{'top': 'small'}}
+            margin={{ 'top': 'small' }}
         >
             <Text alignSelf='center'>
                 Kundvagn
             </Text>
-             {cart.map((cartItem, index) =>
-                <ShoppingItem  
+            {cart.map((cartItem, index) =>
+                <ShoppingItem
                     product={cartItem}
-                    key={index}   
+                    key={index}
                 />
             )}
             <Box
                 direction='row'
                 justify='around'
                 background='#B5BCB0'
-                pad={{'vertical': 'small'}}
+                pad={{ 'vertical': 'small' }}
             >
                 <Text>
-                    
+                    Total:
                 </Text>
                 <Text>
-                    299kr
+                    {getTotalSum(totalSum)} kr
                 </Text>
-            </Box>    
+            </Box>
             <Button
                 label='Gå till kassan'
                 size='small'
                 margin={'small'}
                 color='#708C7E'
                 alignSelf='center'
-                href='/checkOut'  
-            />      
+                href='/checkOut'
+            />
         </Box>
     )
 }
 
-export default ShoppingCart
+export default ShoppingCart;
