d97f783f619d066aa05c2cd44a049c469fcb7d3e
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-03-24 15:52:37 +0100

    ordernummer done

--- src/component/checkOut/orderConfirmation.tsx
+++ src/component/checkOut/orderConfirmation.tsx
@@ -5,16 +5,28 @@ import { OrderContext } from '../../contexts/orderContext';
 function OrderConfirmation( ) {
     const { order } = useContext(OrderContext)
 
+    const printOrderNumber = (min: number, max: number) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // let getgrejer = localStorage.getItem('cart');
+    // getgrejer = JSON.parse('getgrejer');
+
     return (
-        <Box>
+        <Box pad='large'>
             <Text>
+                {/* UNIKT ORDERNUMMER MED RANOMD KANSKE  */}
                 Tack {order.customer.fullname} för din order.
                 Din beställning leveras med {order.deliveryOption}.
-                Betalning sker med {order.paymentMethod}
-                Kortinfo är 
+                Betalning sker med {order.paymentMethod}.
+                Dina kortuppgifter är 
                 {order.cardDetails?.cardNumber} 
                 {order.cardDetails?.expireDate} 
-                {order.cardDetails?.cvcCode}
+                {order.cardDetails?.cvcCode}.
+                Ordernummer: {printOrderNumber(1000, 9999)}
+                {/* {getgrejer} */}
             </Text>
     
         </Box>
