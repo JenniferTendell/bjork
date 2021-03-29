083af5f8cc398db254c1ac05a4ecb8c19d32d117
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-29 17:21:05 +0200

    added animation and styling to error boundary

---
+++ src/component/checkOut/errorBoundary.tsx
@@ -0,0 +1,84 @@
+import { Box, Button, Heading } from "grommet";
+import { Component, ErrorInfo } from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import Lottie from 'react-lottie';
+import animationData from '../assets/errorCat.json'
+
+
+interface Props extends RouteComponentProps{}
+
+interface State {
+    hasError: boolean; 
+}
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+};
+class ErrorBoundary extends Component<Props, State> {
+    
+    state: State = {
+        hasError: false,
+    };
+
+    static getDerivedStateFromError(): State {
+        return { hasError: true}
+    }
+
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+        console.log({ error, errorInfo });
+    }
+
+    reloadPage = () =>  {
+        window.history.back();
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div >
+                    <Box
+                        justify='center'
+                        align='end'
+                        margin={{'top': '10rem'}}
+                    >
+                        <Heading 
+                            level='3'
+                            color='#37513B'
+                        >
+                            Ojdå, något gick fel!</Heading>
+                        <Button 
+                            label='Tillbaka'
+                            size='small'
+                            color='#708C7E'
+                            style={{
+                                'fontWeight': 'normal', 
+                                'background': '#708C7E',
+                                'fontSize': '1.5rem',
+                                'color': 'white',
+                                'padding': '1rem',
+                            }}  
+                            onClick={this.reloadPage}
+                        >
+                        </Button>
+                        <Box pad={{'top': '2rem'}}>
+                        <Lottie
+                            options={defaultOptions}
+                            height={400}
+                            width={400}
+                        />
+                        </Box>
+                    </Box>
+                </div>
+            ); 
+        } 
+        return this.props.children;
+    } 
+}
+
+export default withRouter(ErrorBoundary);
